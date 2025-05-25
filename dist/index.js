a8d49958b74822e8062429912fd5f8630f85901d
Oscar Chavez <oscar.chavez@qsrsoft.com>
2025-05-25 14:54:34 -0700

    test: more debuggin module export errors

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
-// export * from './models/account';
-export * from './models/knowledgeBase';
-export * from './styles/chatCustomization';
-export * from './events/webSocket';
-export * from './events/chatAnalytics';
+export * from './models/account.js';
+export * from './models/knowledgeBase.js';
+export * from './styles/chatCustomization.js';
+export * from './events/webSocket.js';
+export * from './events/chatAnalytics.js';
 // To update the shared types package
 // 1. npn run build
 // 2. npm run bump:patch && npm publish --access public
